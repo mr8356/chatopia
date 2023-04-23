dc7609d778c057b0558267e0ebb979f532178461
HyeongJong Choi <ashapple@ashz.dev>
2023-04-23 12:50:47 +0900

    Update input component

--- chatopia-fe/src/components/AuthInput.vue
+++ chatopia-fe/src/components/AuthInput.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="authinput-container">
-    <input id="authinput-input" :type="type" :placeholder="placeholder" />
-    <span id="authinput-error"></span>
+  <div
+    class="authinput-container"
+    :class="{ 'authinput-container_error': !!errorMessage }"
+  >
+    <input
+      id="authinput-input"
+      :type="type"
+      :placeholder="placeholder"
+      :value="value"
+      @change="inputChanged"
+      @keypress="inputChanged"
+      @input="inputChanged"
+      :required="required"
+    />
+    <span id="authinput-error">{{ errorMessage }}</span>
   </div>
 </template>
 
@@ -13,13 +25,61 @@ export default defineComponent({
   props: {
     type: String,
     placeholder: String,
+    required: Boolean,
+    value: {
+      type: String,
+      default: "",
+    },
+    valid: {
+      type: Boolean,
+      deafult: false,
+    },
+  },
+  emits: ["update:value", "update:valid"],
+  data() {
+    return {
+      isFocus: false,
+      edited: false,
+    };
+  },
+  methods: {
+    inputChanged(evt: Event) {
+      this.edited = true;
+
+      const element = evt.target as HTMLInputElement;
+
+      this.$emit("update:value", element.value || "");
+
+      console.log(element.checkValidity(), element.validity.valid);
+
+      this.$emit("update:valid", element.validity.valid);
+      // this.valid = element.validity.valid;
+    },
+  },
+  computed: {
+    errorMessage() {
+      if (!this.edited) {
+        return "";
+      }
+
+      if (this.valid) {
+        return "";
+      }
+
+      if (this.type === "email") {
+        return "잘못된 이메일 주소";
+      }
+
+      return "잘못된 입력 값";
+    },
   },
 });
 </script>
 
 <style lang="scss">
-#authinput-container {
+.authinput-container {
   display: flex;
+  flex-direction: column;
 }
 
 #authinput-input {
@@ -48,5 +108,13 @@ export default defineComponent({
 
   font-size: 12px;
   height: 14px;
+}
+
+.authinput-container {
+  &.authinput-container_error {
+    #authinput-input {
+      border: 2px solid #e44444;
+    }
+  }
 }
 </style>
